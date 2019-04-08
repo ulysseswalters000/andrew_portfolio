7752de70b232ca98224f08c1da44a30b4885216a
Andrew Walters <ulysseswalters000@gmail.com>
2019-04-08 16:57:20 -0400

    fixed submodule problem

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  div
+    nav.navbar.navbar-expand-lg.navbar-dark.bg-dark
+      router-link.navbar-brand(to="/") AW
+      button.navbar-toggler(type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent")
+        span.navbar-toggler-icon
+      div#navbarContent.collapse.navbar-collapse
+        div.navbar-nav.ml-auto
+          router-link.nav-item.nav-link(to="/") Home
+            span.sr-only (current)
+          router-link.nav-item.nav-link(to="/About") About
+          router-link.nav-item.nav-link(to="/Contact") Contact
+          router-link.nav-item.nav-link(to="/Portfolio") Portfolio
+
+
+</template>
+//- Script
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+
+}
+
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div.home
+    Navbar
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    Navbar,
+    HelloWorld
+  }
+};
+</script>
